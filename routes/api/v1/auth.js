a070e0e42cef3bd2a5923740a76163088fd297e7
Lars Raeymaekers <lars@rmkrs.be>
2024-12-08 13:56:01 +0100

    fix typo

--- routes/api/v1/auth.js
+++ routes/api/v1/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../../../controllers/api/v1/auth");
-const authMiddleware = require("../../../middlewares/auth");
+const authMiddleware = require("../../../middleware/auth");
 
 router.post("/login", authController.login);
 router.put("/change-password", authMiddleware, authController.changePassword);
